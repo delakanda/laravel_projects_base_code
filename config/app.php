a34d4ba6230f727a47b9b3f844bf08b71a2d0f8d
Delali Kanda <delakanda@gmail.com>
2016-05-02 11:20:22 +0000

    changed the forms to the laravel collective forms

--- config/app.php
+++ config/app.php
@@ -136,7 +136,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
         /*
@@ -193,8 +193,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'			=> Illuminate\Html\FormFacade::class,
-		    'HTML'			=> Illuminate\Html\HtmlFacade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
         'Image'     => Intervention\Image\Facades\Image::class,
         'Excel'     => Maatwebsite\Excel\Facades\Excel::class,
 
